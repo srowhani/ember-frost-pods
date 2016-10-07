d2d7d0fc830c0e1dc08e69a38c911563ea10a709
Seena <seenarowhani95@gmail.com>
2016-10-07 13:24:51 -0400

    demo

--- tests/dummy/app/pods/demo/route.js
+++ tests/dummy/app/pods/demo/route.js
@@ -1,16 +1,12 @@
 import Ember from 'ember'
 
-export default Ember.Route.extend({
-  model () {
-    return this.get('store').findAll('dummy-record')
-  },
+const {
+  Route,
+  get
+} = Ember
 
-  setupController (controller, model) {
-    this._super(controller, model)
-    controller.get('podsVisibility').clear()
-//    controller.get('podsVisibility').addObject(Ember.Object.create({
-//      id: 'node',
-//      isPodVisible: false
-//    }))
+export default Route.extend({
+  model () {
+    return get(this, 'store').findAll('dummy-record')
   }
 })
